e67988be2e1fcc4b21d0585c4d75cda16c00a376
lautaropastorino <lautaro.pastorino@hotmail.com>
2020-07-10 21:06:32 -0300

    Agrego generacion de obstaculos

--- GMTK-GameJam/Levels/Cave.gd
+++ GMTK-GameJam/Levels/Cave.gd
@@ -7,8 +7,6 @@ onready var piedra_par = preload("res://Obstacles/PiedraPar.tscn")
 var letras = ["A", "S", "D", "F"]
 var y_min
 var y_max
-var x_max = 480
-var x_min
 
 func _ready():
 	y_min = 400
@@ -22,12 +20,9 @@ func _on_ObstacleSpawn_timeout():
 	for i in range(CANT_OBSTACULOS):
 		if rand_range(0, 1) >= 0.5:
 			piedra = piedra_sola.instance()
-			x_min = 256 
 		else:
-			x_min = 300
 			piedra = piedra_par.instance()
-		piedra.init(letras[rand_range(0, 3)])
-		piedra.set_position(Vector2(rand_range(288, 480), rand_range(y_min, y_max)))
+		piedra.init(letras[rand_range(0, 3)], rand_range(y_min, y_max))
 		add_child(piedra)
 		y_min += 200
 		y_max += 200
--- GMTK-GameJam/Obstacles/Obstaculo.gd
+++ GMTK-GameJam/Obstacles/Obstaculo.gd
@@ -2,15 +2,18 @@ extends Area2D
 class_name Obstaculo
 
 const MAX_POS = 480
-const MIN_POS = 256
+const MIN_POS = 288
+const INIT_POS = {"IZQ": 256, "MID": 370, "DER": 480}
 var letra = ""
 var seleccionar = ""
 const LETRAS = {"A": 65, "S": 83, "D": 68, "F": 70}
 
-func init(l):
+func init(l, ypos):
 	letra = l
 	$Letra.set_text(letra)	
 	seleccionar = LETRAS[l]
+	var xpos = INIT_POS[INIT_POS.keys()[rand_range(0, 3)]]
+	set_position(Vector2(xpos, ypos))
 	
 func _physics_process(delta):
 	if Input.is_key_pressed(seleccionar) and Input.is_action_pressed("move_right"):
